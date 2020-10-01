22a63024769b0d9f249e73a28ece59c7d2227d85
Hashimoto Masahiko <hashimom@geeko.jp>
2020-10-01 18:49:48 +0900

    stream testの追加

--- grpcping/ping.py
+++ grpcping/ping.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import grpc
 from concurrent import futures
+from .pb.grpcpingpb_pb2 import SwapParams
 from .pb.grpcpingpb_pb2 import Status as PbStatus
 from .pb.grpcpingpb_pb2 import Params as PbParams
 from .pb.grpcpingpb_pb2_grpc import gRpcPingServiceServicer, gRpcPingServiceStub, add_gRpcPingServiceServicer_to_server
@@ -10,43 +11,86 @@ from .pb.grpcpingpb_pb2_grpc import gRpcPingServiceServicer, gRpcPingServiceStub
 
 class GrpcPingGw(gRpcPingServiceServicer):
     def Hello(self, request, context):
-        print(" -> receive HELLO from %s" % request.src)
+        print("-> receive HELLO from %s" % request.src)
         return PbStatus(status=200)
+
+    def SwapImage(self, request_iter, context):
+        sending = False
+        i = 0
+        for req in request_iter:
+            if not sending:
+                print("-> echo start")
+                sending = True
+            print(" --> echo: %d" % i)
+            i += 1
+            yield SwapParams(src=req.src, params=req.params)
+        print("-> echo end")
 
 
 class GrpcPing:
-    def __init__(self, self_port=8888):
-        if os.name == 'posix':
-            self._slf_addr = os.uname()[1] + ":" + str(self_port)
-        else:
-            self._slf_addr = "windows_os:" + str(self_port)
+    def __init__(self, src_addr, image=None, grpc_size=1024*1024):
+        self._src_addr = src_addr
+        self._snd_filename = image
+        self._grpc_size = grpc_size
         self._server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
         add_gRpcPingServiceServicer_to_server(GrpcPingGw(), self._server)
-        self._server.add_insecure_port('[::]:' + str(self_port))
+        self._server.add_insecure_port('[::]:' + src_addr.split(':')[1])
         self._server.start()
+        self._cnt = 0
 
     def __call__(self, target):
         with grpc.insecure_channel(target) as channel:
             try:
-                req = PbParams(src=self._slf_addr)
+                req = PbParams(src=self._src_addr)
                 stub = gRpcPingServiceStub(channel)
                 response = stub.Hello(req)
                 if response.status == 200:
                     print("Hello OK")
+
+                    if self._snd_filename is not None:
+                        with open('output' + os.path.splitext(self._snd_filename)[1], 'wb') as f_out:
+                            swap_req_iter = self.SwapReqIter(self._snd_filename, self._src_addr, self._grpc_size)
+                            for res in stub.SwapImage(swap_req_iter, timeout=10):
+                                print(" resp: %d" % self._cnt)
+                                self._cnt += 1
+                                f_out.write(res.params)
+                        print("file output OK!")
+
             except grpc.RpcError as e:
                 print(e)
 
     def __del__(self):
         self._server.stop(0)
 
+    class SwapReqIter(object):
+        def __init__(self, file_name, src_addr, grpc_buf_size):
+            self._src_addr = src_addr
+            self._grpc_buf_size = grpc_buf_size
+            self._f_in = open(file_name, mode='rb')
+            self._f_in.seek(0)
+            self._cnt = 0
+
+        def __iter__(self):
+            return self
+
+        def __next__(self):
+            read_buf = self._f_in.read(self._grpc_buf_size)
+            if not read_buf:
+                self._f_in.close()
+                raise StopIteration()
+            print(" read: %d" % self._cnt)
+            self._cnt += 1
+            return SwapParams(src=self._src_addr, params=read_buf)
+
 
 def main():
     parser = argparse.ArgumentParser(description='grpcping')
-    parser.add_argument('-p', '--port', required=True)
+    parser.add_argument('-s', '--src', required=True)
     parser.add_argument('-t', '--target')
+    parser.add_argument('-i', '--image')
     args = parser.parse_args()
 
-    ping = GrpcPing(args.port)
+    ping = GrpcPing(args.src, args.image)
     if args.target is not None:
         ping(args.target)
     else:
